f55af7a0432e10974cb0b53a08858db81dbc5b66
Dimitris <dddimitriou84@gmail.com>
2016-10-04 07:48:14 +0300

    fixing the indentation

--- math.s
+++ math.s
@@ -1,65 +1,66 @@
-						.text
+      .text
 
-						li			$a0, 3
-						li			$a1, 9
+      li    $a0, 3
+      li    $a1, 9
 						
-						jal			fast
-						move	$a0, $v0
-						jal			printInt
+      jal   fast
+      move  $a0, $v0
+      jal   printInt
 exit:
-						li			$v0, 10
-						syscall
+      li    $v0, 10
+      syscall
 						
 #recursive naive algorithm for power of a number
 #x = b^n	b in $a0 
-#				n in	$a1
-#				x returning value in v0
+#n in	$a1
+#x returning value in v0
+
 power:
-						addi		$sp,	$sp, -8
-						sw		$ra,	  4($sp)
-						sw		$a1,	  8($sp)
+      addi  $sp, $sp, -8
+      sw    $ra, 4($sp)
+      sw    $a1, 8($sp)
 					   
-						bnez		$a1,	rec
-						addi		$sp, $sp, 8		#pop the stack
-						li			$v0, 1				#return 1
-						jr			$ra
+      bnez  $a1, rec
+      addi  $sp, $sp, 8    #pop the stack
+      li    $v0, 1         #return 1
+      jr    $ra
 rec:
-						addi		$a1, $a1 -1		#n = n - 1
-						jal			power
-						mul		$v0,	$v0, $a0 #b * (b^n)
-						lw			$ra,	4($sp)
-						lw			$a1,	8($sp)
-						addi		$sp,	$sp, 8
-						jr			$ra
+      addi  $a1, $a1 -1	   #n = n - 1
+      jal   power
+      mul   $v0, $v0, $a0  #b * (b^n)
+      lw    $ra, 4($sp)
+      lw    $a1, 8($sp)
+      addi  $sp, $sp, 8
+      jr    $ra
 						
 #fast exponetiation algorithm						
 #same as power x = b^n
 #but better fewer multiplications
-#				b in	$a0
-#				n in	$a1
-#				x returning value in v0
+#b in $a0
+#n in $a1
+#x returning value in v0
 						
 fast:
-						addi		$sp, $sp, -8					#free some space of the stack
-						sw		$a1, 4($sp)					#this argument changes, we need to keep it
-						sw		$ra,	8($sp)
+      addi   $sp, $sp, -8    #free some space of the stack
+      sw     $a1, 4($sp)     #this argument changes, we need to keep it
+      sw     $ra, 8($sp)
 						
-						bnez		$a1,	L1						#base case of recursion b^0 = 1
-						addi		$sp, $sp, 8					#pop the stack
-						li			$v0, 1
-						jr			$ra								#return
+      bnez   $a1, L1         #base case of recursion b^0 = 1
+      addi   $sp, $sp, 8     #pop the stack
+      li     $v0, 1
+      jr     $ra             #return
 L1:						
-						addi		$sp, $sp, -4					#we jump to a procedure
-						sw		$ra,	12($sp)				#to 	know if	our number
-						jal			iseven							#is even
-						lw			$ra,	12($sp)				
-						addi		$sp,	$sp, 4
-						beqz		$t0,	L2						#if number is even
-						div		$a1,	$a1, 2					#b^n = (b^(n/2))^2
-						jal			fast
-						mul		$v0,	$v0, $v0				#square the value
+      addi   $sp, $sp, -4    #we jump to a procedure
+      sw     $ra, 12($sp)    #to know if our number
+      jal    iseven	     #is even
+      lw     $ra, 12($sp)    				
+      addi   $sp, $sp, 4     
+      beqz   $t0, L2         #if number is even
+      div    $a1, $a1, 2     #b^n = (b^(n/2))^2
+      jal    fast
+      mul  0,	$v0, $v0	#square the value
 						lw			$ra,	8($sp)					#pop the stack
-						lw			$a1,	4($sp)					#and return
+	lw			$a1,	4($sp)					#and return
 						addi		$sp,	$sp, 8
 						jr			$ra
 L2:																	#if number is not even

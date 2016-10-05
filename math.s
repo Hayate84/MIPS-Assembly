413112309a897527a57324c81d972b5aafa16afa
Dimitris <dddimitriou84@gmail.com>
2016-10-05 09:35:56 +0300

    Update math.s

--- math.s
+++ math.s
@@ -11,11 +11,14 @@
 exit:
       li    $v0, 10
       syscall
-						
-#recursive naive algorithm for power of a number
-#x = b^n	b in $a0 
-#n in	$a1
-#x returning value in v0
+      
+####################################################						
+# recursive naive algorithm for power of a number. #
+# x = b^n                                          # 
+# b in $a0.                                        #   
+# n in $a1                                         #
+# x returning value in v0                          # 
+####################################################
 
 power:
       addi  $sp, $sp, -8
@@ -34,14 +37,16 @@ rec:
       lw    $a1, 8($sp)
       addi  $sp, $sp, 8
       jr    $ra
-						
-#fast exponetiation algorithm						
-#same as power x = b^n
-#but better fewer multiplications
-#b in $a0
-#n in $a1
-#x returning value in v0
-						
+
+#####################################
+# fast exponetiation algorithm.     #						
+# same as power x = b^n.            #
+# but better fewer multiplications. #
+# b in $a0.                         #
+# n in $a1.                         #
+# x returning value in v0.          #
+#####################################
+
 fast:
       addi   $sp, $sp, -8    #free some space of the stack
       sw     $a1, 4($sp)     #this argument changes, we need to keep it
@@ -73,13 +78,11 @@ L2:		             #if number is not even
       lw     $a1, 4($sp)     #and return
       addi   $sp, $sp, 8
       jr     $ra
-      
-square:
-      mul    $a0, $a0 $a0
-      jr     $ra
-      
-#function to check if a number is even
-#is number in $a0 even?
+
+##########################################
+# function to check if a number is even. #
+# is number in $a0 even?                 #
+##########################################
 
 iseven:			
       rem   $t0, $a1, 2    #the psedoinstruction for the rem
@@ -89,8 +92,10 @@ iseven:
 true:			   #else
       li    $t0, 1	   #return 1 for true
       jr    $ra
-						
-#is number in $a0 odd?
+
+########################
+#is number in $a0 odd? #
+########################
 
 odd:
       rem   $a0, $a0, 2
@@ -114,22 +119,22 @@ divNaive:
       sw    $ra, 4($sp)
       sw    $a0, 8($sp)
 					   
-      slt			$t0,	$a0,	$a1			#large < small
-						seq		$t1,	$a0,	$zero		#large == zero
-						or			$t2,  $t0,	$t1			
-					   
-						beqz		$t2,	rec2
-						addi		$sp, $sp, 8				#pop the stack
-						move	$v1, $a0					#the rem of the number is large < small == True
-						li			$v0, 0
-						jr			$ra
+      slt   $t0, $a0, $a1   #large < small
+      seq   $t1, $a0, $zero #large == zero
+      or    $t2, $t0, $t1			
+	  		   
+      beqz  $t2, rec2
+      addi  $sp, $sp, 8     #pop the stack 
+      move  $v1, $a0#the rem of the number is large < small == True
+      li   $v0, 0
+      jr   $ra
 rec2:
-						sub		$a0, $a0, $a1		   #keep subtratring the small number from the large
-						jal			divNaive					
-						addi		$v0, $v0 1			  #the quotiend is, how many time we sutracted the small number from the large
-						lw			$ra,	4($sp)
-						lw			$a0,	8($sp)
-						addi		$sp,	$sp, 8
+      sub	 $a0, $a0, $a1		   #keep subtratring the small number from the large
+      jal			divNaive					
+  				addi		$v0, $v0 1			  #the quotiend is, how many time we sutracted the small number from the large
+     	lw			$ra,	4($sp)
+      lw			$a0,	8($sp)
+     	addi		$sp,	$sp, 8
 						jr			$ra						
 printInt:		
 						addi		$sp, $sp, -4
@@ -138,16 +143,16 @@ printInt:
 						syscall
 						addi		$sp, $sp, -4 		#extra code for the stack
 						sw		$ra, 0($sp)  		#to keep the value of ra
-						jal			printEndl	   		#for printing new line
+				 	jal			printEndl	   		#for printing new line
 						lw			$ra, 0($sp)
-						addi		$sp, $sp, 4
+				 	addi		$sp, $sp, 4
 						jr			$ra
 printEndl:
-						li			$v0, 4
-						move	$t0, $a0   		#usually a0 contains some value
-						la			$a0, endl
-						syscall
-						move	$a0, $t0   		#restore a0 original value
+				 	li			$v0, 4
+				 	move	$t0, $a0   		#usually a0 contains some value
+				 	la			$a0, endl
+					 syscall
+					 move	$a0, $t0   		#restore a0 original value
 						jr			$ra
 	
 						.data

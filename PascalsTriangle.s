29cbf8b3f52e07e8ad14be020f96e060ca461f02
Hayate84 <dddimitriou84@gmail.com>
2016-10-08 16:33:21 +0300

    to show to a friend something

--- PascalsTriangle.s
+++ PascalsTriangle.s
@@ -25,7 +25,7 @@ start:
 			addi	$t1, $t1, -1		#$t1 -= 1 
 			j	start			#loop
 stop:
-			move	$a0, $t0		#restore the original argument value
+			move	$a0, $t0			#restore the original argument value
 loop:							#loop while $a1 > $a0
 			bgt	$a1, $a0, exit		#is $a1 > $a0?
 			jal	binomial		#call the function
